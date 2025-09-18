f6a3aa817d416e368fb2c472f207a21afa148da2
grunkleij <indrajithpg10@gmail.com>
2025-09-18 22:28:22 +0530

    sfs

--- app/src/main/java/com/example/allrecord/MainActivity.java
+++ app/src/main/java/com/example/allrecord/MainActivity.java
@@ -1,24 +1,40 @@
 package com.example.allrecord;
-
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
+    EditText mark1, mark2, mark3;
+    TextView sum ,avg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btn = findViewById(R.id.button);
+        mark1 = findViewById(R.id.m1);
+        mark2 = findViewById(R.id.m2);
+        mark3 = findViewById(R.id.m3);
+        sum = findViewById(R.id.su);
+        avg = findViewById(R.id.av);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int m1 = Integer.parseInt(mark1.getText().toString());
+                int m2 = Integer.parseInt(mark2.getText().toString());
+                int m3 = Integer.parseInt(mark3.getText().toString());
+                int sumr = m1+m2+m3;
+                sum.setText(Integer.toString(sumr));
+                int avgr = sumr/3;
+                avg.setText(Integer.toString(avgr));
+            }
         });
     }
 }
