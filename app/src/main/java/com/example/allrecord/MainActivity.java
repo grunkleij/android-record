96f0ad6e0d4ae718ef7c12dfcc87baaaaa69ae31
grunkleij <indrajithpg10@gmail.com>
2025-09-18 22:42:25 +0530

    dfds

--- app/src/main/java/com/example/allrecord/MainActivity.java
+++ app/src/main/java/com/example/allrecord/MainActivity.java
@@ -1,24 +1,34 @@
 package com.example.allrecord;
-
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
+import android.os.Bundle;
+import android.widget.TextView;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
-
+    private TextView textViewFormat1, textViewFormat2,
+            textViewFormat3, textViewFormat4, textViewFormat5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        textViewFormat1 = findViewById(R.id.textViewFormat1);
+        textViewFormat2 = findViewById(R.id.textViewFormat2);
+        textViewFormat3 = findViewById(R.id.textViewFormat3);
+        textViewFormat4 = findViewById(R.id.textViewFormat4);
+        textViewFormat5 = findViewById(R.id.textViewFormat5);
+        Date currentDate = new Date();
+        SimpleDateFormat format1 = new SimpleDateFormat("EEEE,MMMM dd, yyyy hh:mm:ss a z", Locale.getDefault());
+        textViewFormat1.setText(format1.format(currentDate));
+        SimpleDateFormat format2 = new
+                SimpleDateFormat("MM/dd/yy", Locale.getDefault());
+        textViewFormat2.setText(format2.format(currentDate));
+        SimpleDateFormat format3 = new SimpleDateFormat("MMMM, dd, yyyy", Locale.getDefault());
+                textViewFormat3.setText(format3.format(currentDate));
+        SimpleDateFormat format4 = new SimpleDateFormat("hh:mma", Locale.getDefault());
+                textViewFormat4.setText(format4.format(currentDate));
+        SimpleDateFormat format5 = new
+                SimpleDateFormat("HH:mm:ss", Locale.getDefault());
+        textViewFormat5.setText(format5.format(currentDate));
     }
 }
