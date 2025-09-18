3d194c9ac56956b684674e664356237c1c0a9f9e
grunkleij <indrajithpg10@gmail.com>
2025-09-18 23:04:37 +0530

    dsfds

--- app/src/main/java/com/example/allrecord/MainActivity.java
+++ app/src/main/java/com/example/allrecord/MainActivity.java
@@ -1,24 +1,44 @@
-package com.example.allrecord;
-
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+package com.example.alertdialogapp;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
-
+    private Button showAlertButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        showAlertButton = findViewById(R.id.showAlertButton);
+        showAlertButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                showAlertDialog();
+            }
         });
     }
-}
+    private void showAlertDialog() {
+        AlertDialog.Builder builder = new
+                AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("This is an Alert!");
+        builder.setMessage("Do you want to close this dialog?");
+        builder.setPositiveButton("Yes", new
+                DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        builder.setNegativeButton("No", new
+                DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }}
